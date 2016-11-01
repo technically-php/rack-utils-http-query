bbab79a2cf645c96aa6c4e7658e03030fd04812d
e1himself <ivan.voskoboinyk@gmail.com>
2016-11-01 20:24:46 +0200

    Simplify code by dropping $enc_type argument

--- src/RailsHttpQuery.php
+++ src/RailsHttpQuery.php
@@ -2,7 +2,6 @@
 
 namespace TechnicallyPhp;
 
-use InvalidArgumentException;
 use Traversable;
 
 final class RailsHttpQuery
@@ -11,35 +10,26 @@ final class RailsHttpQuery
      * Generate URL-encoded query string
      *
      * Rails-compatible http_build_query analog.
-     * Please not that this function uses `$enc_type = PHP_QUERY_RFC3986` as default for better interoperability.
-     * This differs from default `http_build_query()` function behavior.
+     * Uses RFC 3986 compliant encoding (see `rawurlencode`).
      *
      * @see http://php.net/manual/en/function.http-build-query.php
+     * @see http://php.net/manual/en/function.rawurlencode.php
      *
      * @param array|object $query_data
      * @param string $arg_separator
-     * @param int $enc_type
      * @return string
      */
-    public static function build($query_data, $arg_separator = '&', $enc_type = PHP_QUERY_RFC3986)
+    public static function build($query_data, $arg_separator = '&')
     {
-        if ($enc_type === PHP_QUERY_RFC1738) {
-            $encode = 'urlencode';
-        } elseif ($enc_type === PHP_QUERY_RFC3986) {
-            $encode = 'rawurlencode';
-        } else {
-            throw new InvalidArgumentException('Unsupported $enc_type given. Only PHP_QUERY_RFC1738 and PHP_QUERY_RFC3986 are supported.');
-        }
-
         $chunks = [];
-        foreach (self::generate($query_data, $encode) as $key => $value) {
+        foreach (self::generate($query_data) as $key => $value) {
             $chunks[] = $key . '=' . $value;
         }
 
         return implode($arg_separator, $chunks);
     }
 
-    private function generate($query_data, $super_var = null, callable $encode)
+    private function generate($query_data, $super_var = null)
     {
         $is_incrementing_sequence = true;
         $expected_numeric_idx = 0;
@@ -48,7 +38,7 @@ final class RailsHttpQuery
             if ($super_var === null) {
                 // if numeric array is given on top-level, use numbers as vars
                 // http_build_query(['a', 'b', 'c']) === '0=a&1=b&2=c'
-                $current_var = $encode((string) $key);
+                $current_var = rawurlencode((string) $key);
 
             } elseif ($is_incrementing_sequence && $key === $expected_numeric_idx) {
                 $expected_numeric_idx++;
@@ -58,7 +48,7 @@ final class RailsHttpQuery
             } else {
                 // numeric sequence break or non-numeric key, use it explicitly
                 $is_incrementing_sequence = false;
-                $current_var = $super_var . '[' . $encode((string) $key) . ']';
+                $current_var = $super_var . '[' . rawurlencode((string) $key) . ']';
             }
 
             if (is_array($value) || $value instanceof Traversable || is_object($value)) {
